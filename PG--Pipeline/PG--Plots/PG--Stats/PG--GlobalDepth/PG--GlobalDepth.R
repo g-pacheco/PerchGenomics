c964a1a20d53e8c98c24882142bcc5d6a12efd48
g-pacheco <ganpa@aqua.dtu.dk>
2022-03-23 14:15:46 -0300

    Changes to GlobalDepth.

--- PG--Pipeline/PG--Plots/PG--Stats/PG--GlobalDepth/PG--GlobalDepth.R
+++ PG--Pipeline/PG--Plots/PG--Stats/PG--GlobalDepth/PG--GlobalDepth.R
@@ -1,6 +1,6 @@
 ### The BEGINNING ~~~~~
 ##
-# ~ Creates FPG--GlobalCoverage | By George PACHECO
+# ~ Creates PG--GlobalDepth | By George Pacheco.
 
 
 # Cleans the environment ~ 
@@ -25,10 +25,11 @@ colnames(Data) <- c("Loci", "Coverage")
 DataUp <- data.frame(Coverage = Data$Coverage, Type = "")
 
 
-# Creates the plot ~
+# Creates plot ~
 GlobalCoverage <-
  ggplot(DataUp, aes(x = Coverage, fill = Type, colour = Type)) +
   geom_density(alpha = .15, adjust = .75, size = .3) +
+  geom_vline(xintercept = 36100, linetype = "longdash", size = .35, color = "#6666ff") +
   scale_x_continuous("Global Depth (X)",
                      breaks = c(5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 55000, 60000, 65000, 70000),
                      labels = c("5K", "10K", "15K","20K", "25K", "30K", "35K", "40K", "45K", "50K", "55K", "60K", "65K","70K"),
@@ -42,7 +43,7 @@ GlobalCoverage <-
   theme(panel.background = element_rect(fill = '#ffffff'),
         panel.grid = element_blank(),
         panel.border = element_blank(),
-        axis.text = element_text(size = 9, color = "#000000"),
+        axis.text = element_text(size = 9, color = "#000000", face = "bold"),
         axis.ticks = element_line(size = .3, color = "#000000"),
         axis.line = element_line(colour = "#000000", size = .3),
         axis.title.x = element_text(size = 15, face = "bold", color = "#000000", margin = margin(t = 20, r = 0, b = 0, l = 0)),
@@ -51,7 +52,10 @@ GlobalCoverage <-
 
   
 # Saves plot ~
-ggsave(GlobalCoverage, file = "PG--GlobalDepth.pdf", width = 12, height = 8, device = cairo_pdf, dpi = 600)
+ggsave(GlobalCoverage, file = "PG--GlobalDepth.pdf",
+       width = 12, height = 8, device = cairo_pdf, dpi = 600)
+ggsave(GlobalCoverage, file = "PG--GlobalDepth.jpeg",
+       width = 12, height = 8, dpi = 300)
 
 
 #
